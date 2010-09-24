3ae53857452a0a61ad5e859f3bd1a23e16da497b
julien <julien.monty@gmail.com>
2010-09-24 14:03:38 +0200

    Fix le probleme de comportement des menus quand déplacement rapide

--- scriptJQuery.js
+++ scriptJQuery.js
@@ -4,28 +4,28 @@ $(window).ready(function(){
         
         $('#menuHaut ul li:eq('+i+')').mouseenter(function(){
             
-            $('#menuHaut ul li a:eq('+i+')').stop(true,false).animate({
+            $('#menuHaut ul li a:eq('+i+')').stop(true,true).animate({
                 'background-color': 'white',
                 'color': 'black',
             },500);
             
-            $('.sousMenu:eq('+i+')').stop(true,false).slideDown(500);
+            $('.sousMenu:eq('+i+')').stop(true,true).slideDown(500);
             
-            $('#voile').stop(true,false).fadeIn(500);
+            $('#voile').stop(true,true).fadeIn(500);
             
         });
 
        
         $('#menuHaut ul li:eq('+i+')').mouseleave(function(){
             
-            $('#menuHaut ul li a:eq('+i+')').css({
+            $('#menuHaut ul li a:eq('+i+')').stop(true,true).animate({
                 'background-color':'black',
                 'color':'white',
-            });
+            },500);
             
-            $('.sousMenu:eq('+i+')').stop(true,false).slideUp(500);
+            $('.sousMenu:eq('+i+')').stop(true,true).slideUp(500);
             
-            $('#voile').stop(true,false).fadeOut(500);
+            $('#voile').stop(true,true).fadeOut(500);
             
         });
 
